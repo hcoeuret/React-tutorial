fffa08ea5a1f1442cf91112fc94266f319521769
hcoeuret <henricoeuret@gmail.com>
2025-04-18 17:16:28 +0200

    Lesson 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import Student from './Student.jsx'
+import UserGreeting from "./UserGreeting.jsx";
 
 function App() {
   return(
     <>
-    <Student name="Spongebob" age={30} isStudent={true}/>
-    <Student name="Patrick" age={42} isStudent={false}/>
-    <Student name="Sandy" age="40" isStudent={false}/>
-    <Student name="Sandy" age={27} isStudent={true}/>
-    <Student name="Larry"/>
+      <UserGreeting isLoggedIn={false} username="Henri"/>
     </>
   );
 }
---
+++ src/UserGreeting.jsx
@@ -0,0 +1,10 @@
+function UserGreeting(props){
+
+  const welcomeMessage =  <h2 className="welcome-message">Welcome {props.username}</h2>
+  const loginMessage =  <h2 className="login-prompt">Please log in to continue</h2>
+
+  return(props.isLoggedIn ? welcomeMessage : loginMessage
+  )
+}
+
+export default UserGreeting
