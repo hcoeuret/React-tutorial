57f5ebfdeeb4eb6777652df5b61b9581331363f9
hcoeuret <henricoeuret@gmail.com>
2025-04-18 17:03:13 +0200

    Lesson 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import Button from "./Button/Button.jsx"
+import Student from './Student.jsx'
 
 function App() {
   return(
     <>
-    <Button/>
+    <Student name="Spongebob" age={30} isStudent={true}/>
+    <Student name="Patrick" age={42} isStudent={false}/>
+    <Student name="Sandy" age="40" isStudent={false}/>
+    <Student name="Sandy" age={27} isStudent={true}/>
+    <Student name="Larry"/>
     </>
   );
 }
