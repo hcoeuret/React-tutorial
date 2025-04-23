6af9cd56aeea3e1d5349a08edcc14075c20e7739
hcoeuret <henricoeuret@gmail.com>
2025-04-23 12:18:55 +0200

    Lesson 14

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -2,25 +2,45 @@ import React, {useState} from "react";
 
 function MyComponent(){
 
-  const [foods, setFoods] = useState(["Apple","Orange","Banana"]);
+  const [cars, setCars] = useState([]);
+  const [carYear, setCarYear] = useState(new Date().getFullYear());
+  const [carMake, setCarMake] = useState("")
+  const [carModel, setCarModel] = useState("")
 
-  function handleAddFood(){
-    const newFood = document.getElementById("foodInput").value;
-    document.getElementById("foodInput").value = "";
-    setFoods(f => [...f, newFood])
+  function handleAddCar(){
+    const newCar = {year : carYear, make: carMake, model: carModel}
+    setCars(c => [...c, newCar])
+    setCarMake("");
+    setCarModel("");
+    setCarYear(new Date().getFullYear());
   }
 
-  function handleRemoveFood(i){
-    setFoods(foods.filter((element, index)=> index !== i));
+  function handleRemoveCar(index){
+    setCars(cars.filter((_, i) => i !== index));
+  }
+
+  function handleYearChange(event){
+    setCarYear(event.target.value);
+  }
+
+  function handleMakeChange(event){
+    setCarMake(event.target.value);
+  }
+
+  function handleModelChange(event){
+    setCarModel(event.target.value);
   }
 
   return(<div>
-    <h2>List of food</h2>
+    <h2>List of Car Objects</h2>
     <ul>
-      {foods.map((f, i) => <li key={i} onClick={() => handleRemoveFood(i)}>{f}</li> )}
+      {cars.map((car, i) => <li key={i} onClick={() => handleRemoveCar(i)}>{car.make} {car.model} {car.year}</li>)}
     </ul>
-    <input type='text' id='foodInput' placeholder="Enter food name"></input>
-    <button onClick={handleAddFood}>Add Food</button>
+
+    <input type="number" value={carYear} onChange={handleYearChange}/><br/>
+    <input type="text" value={carMake} placeholder="Enter Car make" onChange={handleMakeChange}/><br/>
+    <input type="text" value={carModel} placeholder="Enter Car model" onChange={handleModelChange}/><br/>
+    <button onClick={handleAddCar}>Add car</button>
   </div>);
 }
 
